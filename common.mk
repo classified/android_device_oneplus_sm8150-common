b85c59d132ebe75cdd84355205c8a675659b01da
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    TP1803: Create dummy libqti-perfd-client
    * proprietary perfd blobs can be nuked
    * we could map the functions to use libperfmgr powerhints in the future
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- common.mk
+++ common.mk
@@ -319,11 +319,14 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     OPlusFrameworksResCommon
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.msmnile-libperfmgr \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
