ea3324abfa74eda783c4608aaf878ee953cf2d58
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-09 12:52:37 +0300

    sm8150-common: rootdir: Don't configure zram in init.qcom.post_boot.sh
    * Configure it from fstab instead
    
    Change-Id: Ie84d0674a643e637d5ef894399d76ede25d00eeb

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -133,6 +133,14 @@ include $(BUILD_PREBUILT)
 # Common init scripts
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.zram
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.zram
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.oem.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
