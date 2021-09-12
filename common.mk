332b626c948c1ef9f6456a7b7a33fefc08981b7b
Arian <arian.kulmer@web.de>
2021-09-12 19:55:43 +0200

    sm8150-common: power: Import powerhint.json from coral
    https://android.googlesource.com/device/google/coral/+/refs/tags/android-11.0.0_r45/powerhint.json
    
    Change-Id: Ib902d12280364a861105fdfc308fd6682d346688

--- common.mk
+++ common.mk
@@ -373,6 +373,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
