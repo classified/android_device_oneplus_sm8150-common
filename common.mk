54a9a98388fa10045ac475172abd41250e3b649f
LuK1337 <priv.luk@gmail.com>
2021-10-20 19:39:29 +0200

    sm8150-common: Adjust for recent tri state key changes
    tri-state-key package has been integrated into KeyHandler app, and
    KeyHandler itself no longer needs to be added to lineage-sdk overlays.
    
    Change-Id: I13b8f05e8e0b036c9303a01ec0c7e2d99b6756f7

--- common.mk
+++ common.mk
@@ -400,7 +400,8 @@ PRODUCT_PACKAGES += \
 
 # tri-state key
 PRODUCT_PACKAGES += \
-    tri-state-key_daemon.vendor
+    KeyHandler \
+    tri-state-key-calibrate
 
 # Update engine
 PRODUCT_PACKAGES += \
