368b0e2a57552889de28818cafe5ce4db67a98ec
Matheus <mattvieira123@gmail.com>
2021-10-31 22:16:16 -0300

    sm8150-common: kraken init

--- common.mk
+++ common.mk
@@ -19,7 +19,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-krypton
+    $(LOCAL_PATH)/overlay-aospk
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
