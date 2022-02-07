bdc04c206359cb535085de31bf61df853c5f63fb
classified <razorpaul@gmail.com>
2022-02-07 23:21:20 +0530

    sm8150-common: nfc: switch to aosp nfc

--- common.mk
+++ common.mk
@@ -348,12 +348,6 @@ PRODUCT_PACKAGES += \
     NfcNci \
     SecureElement \
     Tag \
-    android.hardware.nfc@1.0.vendor \
-    android.hardware.nfc@1.1.vendor \
-    android.hardware.nfc@1.2.vendor \
-    android.hardware.secure_element@1.0.vendor \
-    android.hardware.secure_element@1.1.vendor \
-    android.hardware.secure_element@1.2.vendor \
     android.hardware.nfc@1.2-service
 
 PRODUCT_COPY_FILES += \
@@ -418,6 +412,12 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
+# Secure Element
+PRODUCT_PACKAGES += \
+    android.hardware.secure_element@1.0.vendor \
+	android.hardware.secure_element@1.1.vendor \
+	android.hardware.secure_element@1.2.vendor
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
