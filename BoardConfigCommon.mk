DerpFest-Devices/device_lenovo_sm8150-common

sm8150-common: Set default the Bluetooth class of device
The class of device is a string with a list of uint8t values:
90,2,12

The meaning is as follows:
{Service Field, Major class, Minor class}

Service Field: 0x5A -> 90
    Bit 17: Networking
    Bit 19: Capturing
    Bit 20: Object Transfer
    Bit 22: Telephony
MAJOR_CLASS: 0x02 -> 2 (Phone)
MINOR_CLASS: 0x0C -> 12 (Smart Phone)

Bug: 217452259
Test: make -j; -- check that bluetooth.device.class_of_device is
correct

Change-Id: I07e83ca352ff969be0e53a0f63b6c940b6c34dfb

BoardConfigCommon.mk
@@ -155,6 +155,7 @@ TARGET_USES_MKE2FS := true
 TARGET_ODM_PROP += $(DEVICE_PATH_COMMON)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH_COMMON)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH_COMMON)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH_COMMON)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH_COMMON)/vendor.prop
 
 # Recovery

classified/android_device_oneplus_sm8150-common

BoardConfigCommon.mk
@@ -51,6 +51,7 @@ TARGET_NO_BOOTLOADER := true
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Bluetooth