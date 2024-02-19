yaap/device_oneplus_sm8150-common

sm8150-common: props: enable and update DPM props
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

BoardConfigCommon.mk
@@ -64,6 +64,7 @@ TARGET_USES_QCOM_BSP := true
 TARGET_ODM_PROP += $(VENDOR_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(VENDOR_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(VENDOR_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(VENDOR_PATH)/vendor.prop
 
 # A/B

classified/android_device_oneplus_sm8150-common

Change-Id: I7fce7fcf3f5a61c6f5e0b9b82bc8bf2508c9b762

BoardConfigCommon.mk
@@ -52,6 +52,7 @@ TARGET_NO_BOOTLOADER := true
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Display