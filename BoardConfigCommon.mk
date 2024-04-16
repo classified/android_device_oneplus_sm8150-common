yaap/device_oneplus_sm8150-common

sm8150-common: Add init script to set properties per variant.
 * Adapted from LineageOS.

Change-Id: Ief5d98e41a5808d057096b25a6e743e608660e12

BoardConfigCommon.mk
@@ -159,6 +159,10 @@ TARGET_PROVIDES_LIBLIGHT := true
 # Media
 TARGET_PROVIDES_LIBPLATFORMCONFIG := true
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_oneplus-sm8150
+TARGET_RECOVERY_DEVICE_MODULES := libinit_oneplus-sm8150
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 

classified/android_device_oneplus_sm8150-common

BoardConfigCommon.mk
@@ -107,6 +107,10 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/sm8150
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := msmnile
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_oneplus-sm8150
+TARGET_RECOVERY_DEVICE_MODULES := libinit_oneplus-sm8150
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 