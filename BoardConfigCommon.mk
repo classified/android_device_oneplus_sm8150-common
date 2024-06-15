78bac3f1885aaa8627799b08887c4de3b8eb4e16
Omkar Chandorkar <gotenksIN@aosip.dev>
2024-06-15 14:24:11 +0300

    sm8150-common: Properly configure FCM for A14/QPR3
    Change-Id: Idfbf1c03017ac0cf45084fa49108e407d79840dd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,7 +82,8 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oneplus:libudfps_extension.oneplus
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml \
+    $(COMMON_PATH)/oneplus_vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest-qva.xml
